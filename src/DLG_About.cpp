a114c93eaa8a0d915b1fdf786e5d0c9625c5be88
HuYingzhuo(hugo) <hyzboy@outlook.com>
2025-12-13 22:54:11 +0800

    About对话框增加内容

--- src/DLG_About.cpp
+++ src/DLG_About.cpp
@@ -24,14 +24,25 @@ DLG_About::DLG_About(QWidget *parent)
     QVBoxLayout *layout = new QVBoxLayout(this);
 
     QLabel *titleLabel = new QLabel("Git Repository Manager", this);
-    QFont titleFont = titleLabel->font();
-    titleFont.setPointSize(16);
-    titleFont.setBold(true);
-    titleLabel->setFont(titleFont);
-    titleLabel->setAlignment(Qt::AlignCenter);
+    {
+        QFont titleFont = titleLabel->font();
+        titleFont.setPointSize(16);
+        titleFont.setBold(true);
+        titleLabel->setFont(titleFont);
+        titleLabel->setAlignment(Qt::AlignCenter);
+    }
 
-    QLabel *verLabel = new QLabel(QString("version: 0.1"), this);
+    QLabel *verLabel = new QLabel(QString("Version: 0.1"), this);
     verLabel->setAlignment(Qt::AlignCenter);
+
+    QLabel *authorLabel = new QLabel(QString("Author: YingzhuoHu"), this);
+    {
+        authorLabel->setAlignment(Qt::AlignCenter);
+        //QFont authorFont=authorLabel->font();
+        //authorFont.setBold(true);
+        //authorLabel->setFont(authorFont);
+        authorLabel->setStyleSheet("color: yellow; font-weight: bold;");
+    }
 
     QLabel *qtLabel = new QLabel(QString("Qt version: %1").arg(QT_VERSION_STR), this);
     qtLabel->setAlignment(Qt::AlignCenter);
@@ -39,16 +50,21 @@ DLG_About::DLG_About(QWidget *parent)
     QLabel *git2Label = new QLabel(libgit2Version, this);
     git2Label->setAlignment(Qt::AlignCenter);
 
-    QLabel *statusLabel = new QLabel("✓ Qt6 and libgit2 successfully integrated!", this);
-    statusLabel->setAlignment(Qt::AlignCenter);
-    statusLabel->setStyleSheet("color: green; font-weight: bold;");
+    QLabel *repoLinkLabel = new QLabel("<a href=\"https://github.com/hyzboy/GitRepoManager.git\">https://github.com/hyzboy/GitRepoManager.git</a>", this);
+    {
+        repoLinkLabel->setAlignment(Qt::AlignCenter);
+        repoLinkLabel->setTextFormat(Qt::RichText);
+        repoLinkLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        repoLinkLabel->setOpenExternalLinks(true);
+    }
 
     layout->addStretch();
     layout->addWidget(titleLabel);
+    layout->addWidget(authorLabel);
     layout->addWidget(verLabel);
     layout->addWidget(qtLabel);
     layout->addWidget(git2Label);
-    layout->addWidget(statusLabel);
+    layout->addWidget(repoLinkLabel);
     layout->addStretch();
 }
 
